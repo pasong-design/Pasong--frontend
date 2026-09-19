613fba60bf0bd8b6cd78631e6350a10d971c98e0
pasong-design <kivumbilowrence652@gmail.com>
2026-09-19 16:05:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,54 @@
-const CACHE_NAME = "pasong-v1";
+const CACHE_NAME = "pasong-v2";
+
+const APP_SHELL = [
+"/",
+"/index.html",
+"/manifest.json",
+"/icons/icon-192.png",
+"/icons/icon-512.png"
+];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(APP_SHELL);
+})
+);
+
+self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+event.waitUntil(
+caches.keys().then(cacheNames => {
+return Promise.all(
+cacheNames
+.filter(name => name !== CACHE_NAME)
+.map(name => caches.delete(name))
+);
+})
+);
+
+self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+if (event.request.method !== "GET") return;
 
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+event.respondWith(
+fetch(event.request)
+.then(response => {
+const responseClone = response.clone();
+
+    caches.open(CACHE_NAME).then(cache => {
+      cache.put(event.request, responseClone);
+    });
+
+    return response;
+  })
+  .catch(() => {
+    return caches.match(event.request);
+  })
+
+);
 });
