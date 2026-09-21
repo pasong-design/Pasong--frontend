fef35cf90da4985da49758f262c9b5eaf4a85088
pasong-design <kivumbilowrence652@gmail.com>
2026-09-21 21:24:16 +0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = "https://mtufczmjlkvycarxylgh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_XDIZ0_nd4PAYJy2Oz4VaxQ_R4Trs3VP";
-let pasongSupabase = null;
-if(window.supabase){ pasongSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY); }
-// ... paste REST of your JS here ...
